b9fb91c7b3eeb6f7ae51d99309ca9fe2f9e3d094
Tharouat <habka.tharouat@gmail.com>
2024-01-30 18:58:42 +0100

    my fifa card

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import React from "react";
 import PlayersList from "./PlayersList";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -13,15 +11,8 @@ function App() {
         <h1>FIFA Player Cards</h1>
         <PlayersList />
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    
+   </>
   )
 }
 
